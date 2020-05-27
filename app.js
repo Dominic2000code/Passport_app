1c76368a82c1e5b076e29cd56a91957d23409d3a
Bluawofogbe Dominic <bluawofogbedominic@gmail.com>
2020-05-27 03:30:12 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+const app = express();
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
+//Mongo Connection
+
+mongoose
+	.connect("mongodb://localhost/users", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("MongoDB Connected...");
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
+//EJS middleware
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+//frontend frameworks
+app.use(
+	"/css",
+	express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
+app.use(
+	"/js",
+	express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+);
+
+//Express session middleware
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+	session({
+		secret: "secret",
+		resave: true,
+		saveUninitialized: true,
+		cookie: { secure: true },
+	})
+);
+
+// connect flash
+app.use(flash());
+
+//Global variables 
+app.use( (req, res, next)=> {
+	res.locals.success_msg = req.flash("success_msg");
+	res.locals.error_msg = req.flash("error_msg");
+	res.locals.error = req.flash("error");
+	next();
+});
+
+//Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ routes/users.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+//User model
+const User = require("../models/User");
+
+//Login page
+router.get("/login", (req, res) => {
+	res.render("login");
+});
+
+//Register page
+router.get("/register", (req, res) => {
+	res.render("register");
+});
+
+//Registration Post Handler
+router.post("/register", (req, res) => {
+	const { name, email, password, password2 } = req.body;
+	let errors = [];
+
+	//Check required fields
+	if (!name || !email || !password || !password2) {
+		errors.push({ msg: "Please fill in all fields" });
+	}
+
+	if (password !== password2) {
+		errors.push({ msg: "Passwords do not match" });
+	}
+
+	//Check password length
+	if (password.length < 6) {
+		errors.push({ msg: "Password must be at least 6 characters long" });
+	}
+
+	if (errors.length > 0) {
+		res.render("register", {
+			errors,
+			name,
+			email,
+			password,
+			password2,
+		});
+	} else {
+		//validating user
+		User.findOne({ email: email }).then((user) => {
+			if (user) {
+				// if User exist
+				errors.push({ msg: "Email already exist" });
+				res.render("register", {
+					errors,
+					name,
+					email,
+					password,
+					password2,
+				});
+			} else {
+				const newUser = new User({
+					name,
+					email,
+					password,
+				});
+
+				//Hashing Password
+				bcrypt.genSalt(10, (error, salt) =>
+					bcrypt.hash(newUser.password, salt, (error, hash) => {
+						if (error) throw error;
+
+						//setting password to hash
+						newUser.password = hash;
+
+						//save user
+						newUser
+							.save()
+							.then((user) => {
+								req.flash(
+									"success_msg",
+									"You are now registered and can now sign in"
+								);
+								res.redirect("/users/login");
+								// console.log(newUser)
+							})
+							.catch((error) => console.log(error));
+					})
+				);
+			}
+		});
+	}
+});
+
+module.exports = router;
